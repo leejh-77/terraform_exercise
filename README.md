87d7f87523815eba72e7c383a43ad69dab66f2ad
jonghoon <>
2022-05-15 16:25:41 +0900

    Add EC2 Resource

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Terraform 실습
+
+다음의 포스트를 따라 실습  
+https://www.44bits.io/ko/post/terraform_introduction_infrastrucute_as_code
+
+### 주요 커맨드
+terraform init - 현재 선언된 provider 의 플러그인을 설치한다.  
+terraform plan - 이전의 리소스와 현재 변경된 리소스를 비교하여 표시한다. add, change, destroy  
+terraform apply - 정의된 리소스를 반영한다. plan 이 먼저 실행되고 yes 를 입력하면 실제 반영.
--- web_infra.tf
+++ web_infra.tf
@@ -1,4 +1,32 @@
+# Key pair
 resource "aws_key_pair" "web_admin" {
-    key_name = "web_admin"
-    public_key = file("~/.ssh/web_admin.pub")
+    key_name = "web_admin" // aws 내에서 사용할 리소스 이름
+    public_key = file("~/.ssh/web_admin.pub") // public key path 지정
+}
+
+# Security Group
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh_from_all"
+  description = "Allow SSH port from all"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_security_group" "default" {
+    name = "default"
+}
+
+# EC2
+resource "aws_instance" "web" {
+  ami = "ami-0cbec04a61be382d9" # amzn2-ami-hvm-2.0.20200207.1-x86_64-gp2
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.web_admin.key_name
+  vpc_security_group_ids = [
+    aws_security_group.ssh.id,
+    data.aws_security_group.default.id
+  ]
 }
